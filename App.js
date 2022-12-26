1b18b83248ffe3607b523aded632bb79b00c3de9
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-12-26 10:59:48 -0800

    init navigation and importing screens

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./screens/Home";
+import Settings from "./screens/Settings";
+import InGame from "./screens/InGame";
+import EndGame from "./screens/EndGame";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="HomeScreen">
+				<Stack.Screen name="SettingsScreen" component={Settings} />
+				<Stack.Screen name="HomeScreen" component={Home} />
+				<Stack.Screen name="InGameScreen" component={InGame} />
+				<Stack.Screen name="EndGameScreen" component={EndGame} />
+			</Stack.Navigator>
+			<StatusBar style="auto" />
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
