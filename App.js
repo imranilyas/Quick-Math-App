6b3802e7c36009f2b4627395bcb77c41e5391d5c
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-12-29 13:18:56 -0800

    basic redux and store added

--- App.js
+++ App.js
@@ -8,19 +8,32 @@ import Settings from "./screens/Settings";
 import InGame from "./screens/InGame";
 import EndGame from "./screens/EndGame";
 
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName="HomeScreen">
-				<Stack.Screen name="SettingsScreen" component={Settings} />
-				<Stack.Screen name="HomeScreen" component={Home} />
-				<Stack.Screen name="InGameScreen" component={InGame} />
-				<Stack.Screen name="EndGameScreen" component={EndGame} />
-			</Stack.Navigator>
+		<>
 			<StatusBar style="auto" />
-		</NavigationContainer>
+			<Provider store={store}>
+				<NavigationContainer>
+					<Stack.Navigator initialRouteName="HomeScreen">
+						<Stack.Screen
+							name="SettingsScreen"
+							component={Settings}
+						/>
+						<Stack.Screen name="HomeScreen" component={Home} />
+						<Stack.Screen name="InGameScreen" component={InGame} />
+						<Stack.Screen
+							name="EndGameScreen"
+							component={EndGame}
+						/>
+					</Stack.Navigator>
+				</NavigationContainer>
+			</Provider>
+		</>
 	);
 }
 
---
+++ redux/settingsSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const settingsSlice = createSlice({
+	name: "settings",
+	initialState: {
+		difficulty: "easy",
+		darkMode: false,
+		rounds: 10,
+		roundProgress: 0,
+	},
+	reducers: {
+		incrementRound: (state) => {
+			state.roundProgress += 1;
+		},
+
+		resetRound: (state) => {
+			state.roundProgress = 0;
+		},
+
+		setRounds: (state, action) => {
+			state.rounds = action.payload;
+		},
+
+		appearance: (state, action) => {
+			state.darkMode = action.payload;
+		},
+
+		setDifficulty: (state, action) => {
+			state.difficulty = action.payload;
+		},
+	},
+});
+
+export const incrementRound = settingsSlice.actions.incrementRound;
+export const resetRound = settingsSlice.actions.resetRound;
+export const setRounds = settingsSlice.actions.setRounds;
+export const appearance = settingsSlice.actions.appearance;
+export const setDifficulty = settingsSlice.actions.setDifficulty;
+
+export default settingsSlice.reducer;
