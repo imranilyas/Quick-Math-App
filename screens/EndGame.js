64d1b8a26a58b7a95a0de786a208cf1033bd7455
imranilyas <68575807+imranilyas@users.noreply.github.com>
2023-03-03 10:58:32 -0800

    styling + addition - button on end screen aligned to bottom + correct and incorrect expressions filtered

--- screens/EndGame.js
+++ screens/EndGame.js
@@ -5,16 +5,24 @@ import GameSelectButton from "../components/GameSelectButton";
 
 const EndGame = () => {
 	const navigation = useNavigation();
+
 	const expressions = useSelector((state) => state.settings.expressions);
-	console.log(expressions);
+	const correct = expressions.filter((item) => item.choice === true);
+	const incorrect = expressions.filter((item) => item.choice === false);
+
 	const returnHandler = () => {
 		navigation.navigate("HomeScreen");
 	};
 
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text>End-Game Screen</Text>
-			<GameSelectButton onPress={returnHandler}>
+			<GameSelectButton
+				onPress={returnHandler}
+				outerStyle={styles.buttonContainer}
+				innerStyle={styles.buttonInnerContainer}
+				textStyle={styles.buttonText}
+			>
 				Main Menu
 			</GameSelectButton>
 		</View>
@@ -22,7 +30,33 @@ const EndGame = () => {
 };
 
 const styles = StyleSheet.create({
-	container: {},
+	container: {
+		margin: 15,
+		flex: 1,
+	},
+
+	buttonContainer: {
+		marginVertical: 15,
+		marginHorizontal: "3%",
+		flex: 1,
+		justifyContent: "flex-end",
+	},
+
+	buttonInnerContainer: {
+		backgroundColor: "#cccccc",
+		borderRadius: "8%",
+		shadowColor: "black",
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 1,
+		shadowOpacity: 0.5,
+	},
+
+	buttonText: {
+		padding: 10,
+		fontSize: 20,
+		fontWeight: "bold",
+		textAlign: "center",
+	},
 });
 
 export default EndGame;
